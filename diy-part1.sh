1efa1bba18e95248746ca16bbec2b5d4da871164
banri1 <139927187+banri1@users.noreply.github.com>
2026-02-02 02:33:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # 添加 PassWall 軟體源
-echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git" >> feeds.conf.default
+
 
 
 
